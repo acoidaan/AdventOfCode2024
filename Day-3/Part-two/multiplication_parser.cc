c98ab9eb84f65de6bb8292b4d78e2685ab1d3a13
Acoidan Martín Conrado <alu0101623627@ull.edu.es>
2024-12-03 16:49:43 +0000

    Refactor MultiplicationParser to use a stack for enabling/disabling sections

--- Day-3/Part-two/multiplication_parser.cc
+++ Day-3/Part-two/multiplication_parser.cc
@@ -1,39 +1,39 @@
 #include <fstream>
 #include <iostream>
 #include <regex>
+#include <stack>
 #include <string>
 
 class MultiplicationParser {
-  static bool enabled;
+  static std::stack<bool> enabledStack;
 
  public:
-  static void reset() { enabled = true; }
+  static void reset() {
+    while (!enabledStack.empty()) enabledStack.pop();
+    enabledStack.push(true);
+  }
+
+  static bool isEnabled() {
+    return !enabledStack.empty() && enabledStack.top();
+  }
 
   static int ProcessInput(const std::string& input) {
     int total_sum = 0;
     size_t pos = 0;
+    bool inDisabledSection = false;
 
     while (pos < input.length()) {
-      auto isValidCommand = [&](size_t p) {
-        return p == 0 || !isalnum(input[p - 1]);
-      };
-
       if (pos + 4 <= input.length() && input.substr(pos, 4) == "do()" &&
-          isValidCommand(pos)) {
-        enabled = true;
+          (pos == 0 || !isalnum(input[pos - 1]))) {
+        inDisabledSection = false;
         pos += 4;
-        continue;
-      }
-
-      if (pos + 7 <= input.length() && input.substr(pos, 7) == "don't()" &&
-          isValidCommand(pos)) {
-        enabled = false;
+      } else if (pos + 7 <= input.length() &&
+                 input.substr(pos, 7) == "don't()" &&
+                 (pos == 0 || !isalnum(input[pos - 1]))) {
+        inDisabledSection = true;
         pos += 7;
-        continue;
-      }
-
-      if (pos + 4 <= input.length() && input.substr(pos, 4) == "mul(" &&
-          isValidCommand(pos)) {
+      } else if (pos + 4 <= input.length() && input.substr(pos, 4) == "mul(" &&
+                 (pos == 0 || !isalnum(input[pos - 1]))) {
         size_t end = input.find(')', pos);
         if (end != std::string::npos) {
           std::string nums = input.substr(pos + 4, end - (pos + 4));
@@ -49,22 +49,25 @@ class MultiplicationParser {
 
             if (std::regex_match(num1, std::regex("\\d{1,3}")) &&
                 std::regex_match(num2, std::regex("\\d{1,3}"))) {
-              if (enabled) {
-                total_sum += std::stoi(num1) * std::stoi(num2);
+              if (!inDisabledSection) {
+                total_sum +=
+                    static_cast<int64_t>(std::stoi(num1)) * std::stoi(num2);
               }
             }
           }
           pos = end + 1;
-          continue;
+        } else {
+          pos++;
         }
+      } else {
+        pos++;
       }
-      pos++;
     }
     return total_sum;
   }
 };
 
-bool MultiplicationParser::enabled = true;
+std::stack<bool> MultiplicationParser::enabledStack;
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -84,7 +87,6 @@ int main(int argc, char* argv[]) {
   MultiplicationParser::reset();
   int64_t total = MultiplicationParser::ProcessInput(content);
 
-  std::cout << "La suma es de todas las multiplicaciones habilitadas es: "
-            << total << std::endl;
+  std::cout << "Total: " << total << std::endl;
   return 0;
 }
